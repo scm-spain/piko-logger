360abd827a661bdf68d33c1d9a4f293e95c07508
maryam.malek <maryam.malek@adevinta.com>
2020-05-25 07:31:33 +0200

    add standrad log levels

--- src/main/domain/constants.js
+++ src/main/domain/constants.js
@@ -1,7 +1,12 @@
 export const LOCAL_STORAGE_KEY = 'piko.level'
 export const DEFAULT_LEVEL = 'off'
 export const LEVEL = {
-  debug: {id: 1, label: 'DEBUG'},
-  error: {id: 2, label: 'ERROR'},
-  off: {id: 3, label: 'OFF'}
+  all: {id: 1, label: 'ALL'},
+  trace: {id: 2, label: 'TRACE'},
+  debug: {id: 3, label: 'DEBUG'},
+  info: {id: 4, label: 'INFO'},
+  warn: {id: 5, label: 'WARN'},
+  error: {id: 6, label: 'ERROR'},
+  fatal: {id: 7, label: 'FATAL'},
+  off: {id: 8, label: 'OFF'}
 }
